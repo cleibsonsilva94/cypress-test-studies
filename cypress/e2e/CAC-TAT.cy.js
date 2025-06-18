789a9325bb198f50fe6b158fa4fe4fc2254a3f40
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-18 16:36:58 -0300

    First exercise done after reading part of the suggested documentation

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+// Primeiro exercício feito por mim depois de ler a documentação
+
+describe('Central de Atendimento ao Cliente TAT', () => { // Suíte de testes
+  it('verifica o título da aplicação', () => { // Caso de teste
+    cy.visit('./src/index.html')
+    cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
   })
 })
