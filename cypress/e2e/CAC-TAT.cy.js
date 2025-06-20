e1a77163316115422ac111e2eee0f7c8299655fb
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-20 08:54:31 -0300

    Post correction of the first exercise

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,8 +1,8 @@
-// Primeiro exercício feito por mim depois de ler a documentação
+// Primeiro exercício feito por mim após ler a documentação
 
 describe('Central de Atendimento ao Cliente TAT', () => { // Suíte de testes
-  it('verifica o título da aplicação', () => { // Caso de teste
+  it('verifica o título da aplicação', () => { // Caso de teste sempre definido por "it"
     cy.visit('./src/index.html')
-    cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+    cy.title().should('not.be.equal', 'Central de Atendimento ao Cliente TAT') // O primeiro argumento do "should" é o comando, como "be.equal". Para negar uma asserção, podemos usar "not" antes do comando :)
   })
 })
