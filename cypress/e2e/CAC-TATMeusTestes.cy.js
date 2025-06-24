12d249258d796316dacf5281facaf62af98294c5
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-24 11:57:07 -0300

    Reorganizing code and applying some of the teacher's suggestions and methodologies

--- cypress/e2e/CAC-TATMeusTestes.cy.js
+++ cypress/e2e/CAC-TATMeusTestes.cy.js
@@ -2,8 +2,7 @@ describe('CT001 Central de Atendimento ao Cliente TAT', () => {
   beforeEach(() => {
     cy.visit('./src/index.html')
   })
-
-  it('CT 001 preenche os campos obrigatórios e envia o formulário', () => {
+  it('CT001 - Preenche os campos obrigatórios e envia o formulário', () => {
     cy.get('input[name="firstName"]')
       .as('firstNameField')
       .should('be.visible')
@@ -35,51 +34,55 @@ describe('CT001 Central de Atendimento ao Cliente TAT', () => {
       .should('be.visible')
       .type('Botafogo venceu')
     cy.get('@elogiolField').should('have.value', 'Obrigado')
-    cy.get('.button').click()
 
+    cy.get('.button').click()
     cy.get('.success')
   })
 
-  it('CT002 Escrevendo uma mensagem de feedback com muitos caracteres para testar o delay.', () => {
+  it('CT002 - Escrevendo uma mensagem de feedback com muitos caracteres para testar o delay.', () => {
     cy.get('#firstName').type('Walmyr')
     cy.get('#lastName').type('Lima e Silva Filho')
     cy.get('#email').type('walmyr@talkingabouttesting.com')
-    cy.get('#open-text-area').type('Digno és, Senhor, de receber glória, e honra, e poder; porque tu criaste todas as coisas, e por tua vontade são e foram criadas', { delay: 0 })
+    cy.get('#open-text-area').type(
+      'Digno és, Senhor, de receber glória, e honra, e poder; porque tu criaste todas as coisas, e por tua vontade são e foram criadas',
+      { delay: 0 }
+    )
     cy.get('button[type="submit"]').click()
-
     cy.get('.success')
   })
 
-  it('CT003 Teste que usa o contains para preencher campos.', () => {
+  it('CT003 - Teste que usa o contains para preencher campos.', () => {
     cy.contains('label', 'Nome').type('Walmyr')
     cy.contains('label', 'Sobrenome ').type('Lima e Silva Filho')
     cy.contains('label', 'E-mail').type('walmyr@talkingabouttesting.com')
-    cy.contains('label', 'Algum elogio ou feedback para nós?').type('Digno és, Senhor, de receber glória, e honra, e poder; porque tu criaste todas as coisas, e por tua vontade são e foram criadas', { delay: 0 })
+    cy.contains('label', 'Algum elogio ou feedback para nós?').type(
+      'Digno és, Senhor, de receber glória, e honra, e poder; porque tu criaste todas as coisas, e por tua vontade são e foram criadas',
+      { delay: 0 }
+    )
     cy.contains('button', 'Enviar').click()
-
     cy.get('.success').should('be.visible')
   })
-  it('CT004 Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário"', () => {
+
+  it('CT004 - Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido', () => {
     cy.get('#firstName').type('Walmyr')
     cy.get('#lastName').type('Lima e Silva Filho')
     cy.get('#email').type('walmyr@talkingabouttesting.com')
     cy.get('#phone-checkbox').click()
     cy.get('button[type="submit"]').click()
-
     cy.get('.error').should('be.visible')
   })
-  it('CT005 Teste que verifica a impossibilidade de enviar o formulário sem preencher nada.', () => {
+
+  it('CT005 - Verifica a impossibilidade de enviar o formulário sem preencher nada.', () => {
     cy.get('button[type="submit"]').click()
-
     cy.get('.error').should('be.visible')
   })
-  it('CT006 Validação do campo "telefone": inserção de strings em vez de números"', () => {
-    cy.get('#phone')
-      .type('abc')
 
+  it('CT006 - Validação do campo "telefone": inserção de strings em vez de números', () => {
+    cy.get('#phone').type('abc')
     cy.get('input').should('not.have.value', 'abc')
   })
-  it('CT007 Verifica se os campos do formulário são apagados corretamente.', () => {
+
+  it('CT007 - Verifica se os campos do formulário são apagados corretamente.', () => {
     cy.get('#firstName')
       .type('WA')
       .should('have.value', 'WA')
@@ -88,65 +91,53 @@ describe('CT001 Central de Atendimento ao Cliente TAT', () => {
       .type('Walmyr')
       .should('have.value', 'Walmyr')
 
-    cy.get('#lastName').type('lima e silva filho').should('have.value', 'lima e silva filho').clear().should('not.have.value', 'lima e silva filho').type('Lima e Silva Filho').should('have.value', 'Lima e Silva Filho')
-    cy.get('#email').type('WALMYE@talkingabouttesting.com').should('have.value', 'WALMYE@talkingabouttesting.com').clear().should('not.have.value', 'WALMYE@talkingabouttesting.com').type('walmye@talkingabouttesting.com').should('have.value', 'walmye@talkingabouttesting.com')
-    cy.get('#phone').type('123456789').should('have.value', '123456789').clear().should('not.have.value', '123456789').type('897654321').should('have.value', '897654321')
+    cy.get('#lastName')
+      .type('lima e silva filho')
+      .should('have.value', 'lima e silva filho')
+      .clear()
+      .should('not.have.value', 'lima e silva filho')
+      .type('Lima e Silva Filho')
+      .should('have.value', 'Lima e Silva Filho')
 
+    cy.get('#email')
+      .type('WALMYE@talkingabouttesting.com')
+      .should('have.value', 'WALMYE@talkingabouttesting.com')
+      .clear()
+      .should('not.have.value', 'WALMYE@talkingabouttesting.com')
+      .type('walmye@talkingabouttesting.com')
+      .should('have.value', 'walmye@talkingabouttesting.com')
+
+    cy.get('#phone')
+      .type('123456789')
+      .should('have.value', '123456789')
+      .clear()
+      .should('not.have.value', '123456789')
+      .type('897654321')
+      .should('have.value', '897654321')
   })
 
-  // A aplicação de comandos customizados no Cypress é semelhante ao padrão PageObject.
-  // A principal diferença é que não há necessidade de importar manualmente as classes com os métodos
-  // específicos de cada página em todos os arquivos de teste. 
-  // Ao criar um comando customizado, ele é automaticamente incluído no objeto `cy`.
-  // Basta referenciar o arquivo que contém os comandos dentro do `e2e.js` no momento da criação.
-  // A ideia e o princípio são os mesmos do PageObject, porém com a vantagem de deixar o código mais limpo.
-  it.only('fills the form', () => {
+  it.only('CT008 - Preenche o formulário com comando customizado', () => {
     cy.fillSignupForm()
   })
 })
 
-/*
-Sugestão de outra abordagem para o CT006: basicamente, separar as interações por campo para melhorar a legibilidade.
-it.only('CT007 - Verifica se os campos do formulário podem ser apagados e reescritos.', () => {
-  // Primeiro Nome
-  cy.get('#firstName')
-    .type('WA')
-    .should('have.value', 'WA')
-    .clear()
-    .should('have.value', '') // mais direto
-    .type('Walmyr')
-    .should('have.value', 'Walmyr')
+/* ===================================
+   COMENTÁRIOS ADICIONAIS E EXPLICATIVOS
+====================================== */
 
-  // Sobrenome
-  cy.get('#lastName')
-    .type('lima e silva filho')
-    .should('have.value', 'lima e silva filho')
-    .clear()
-    .should('have.value', '')
-    .type('Lima e Silva Filho')
-    .should('have.value', 'Lima e Silva Filho')
+// A aplicação de comandos customizados no Cypress é semelhante ao padrão PageObject.
+// A principal diferença é que não há necessidade de importar manualmente as classes com os métodos
+// específicos de cada página em todos os arquivos de teste. 
+// Ao criar um comando customizado, ele é automaticamente incluído no objeto `cy`.
+// Basta referenciar o arquivo que contém os comandos dentro do `e2e.js` no momento da criação.
+// A ideia e o princípio são os mesmos do PageObject, porém com a vantagem de deixar o código mais limpo.
 
-  // E-mail
-  cy.get('#email')
-    .type('WALMYE@talkingabouttesting.com')
-    .should('have.value', 'WALMYE@talkingabouttesting.com')
-    .clear()
-    .should('have.value', '')
-    .type('walmye@talkingabouttesting.com')
-    .should('have.value', 'walmye@talkingabouttesting.com')
+/* 
+Sugestão de reestruturação para o CT007:
+Separar as ações por campo melhora a clareza do teste. Também é possível criar uma função
+reutilizável que recebe o seletor e dois valores (para preencher, apagar e preencher de novo).
 
-  // Telefone
-  cy.get('#phone')
-    .type('123456789')
-    .should('have.value', '123456789')
-    .clear()
-    .should('have.value', '')
-    .type('897654321')
-    .should('have.value', '897654321')
-
-Uma boa prática também é usar uma função para isso e deixá-la na mesma classe ou em um arquivo separado, dependendo da organização do proj
-    
-})
+Exemplo de função:
 
 export function preencherApagarPreencher(selector, valor1, valor2) {
   cy.get(selector)
@@ -157,5 +148,4 @@ export function preencherApagarPreencher(selector, valor1, valor2) {
     .type(valor2)
     .should('have.value', valor2)
 }
-
 */
