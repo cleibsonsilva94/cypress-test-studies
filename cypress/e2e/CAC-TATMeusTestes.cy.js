66cbf7ef7787e849904555ec4f5d7c98e296cf51
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-25 15:26:34 -0300

    Test CT007 simulating drag and drop with second argument 'action' as specified in the implementation

--- cypress/e2e/CAC-TATMeusTestes.cy.js
+++ cypress/e2e/CAC-TATMeusTestes.cy.js
@@ -175,7 +175,15 @@ describe('CT001 Central de Atendimento ao Cliente TAT', () => {
   })
   it('CT016 - Enviando um arquivo', () => {
     cy.get('#file-upload')
-    .selectFile('cypress/fixtures/example.json')
+      .selectFile('cypress/fixtures/example.json')
+      .should(input => {
+        expect(input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it.only('CT017 - Enviando um arquivo', () => {
+    cy.get('#file-upload')
+      .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
       .should(input => {
         expect(input[0].files[0].name).to.equal('example.json')
       })
