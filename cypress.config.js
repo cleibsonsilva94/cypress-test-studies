04e7b4461a73c502a5c050a54540b99a4f1a271c
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-27 10:27:48 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -7,9 +7,13 @@ const { defineConfig } = require('cypress')
 //})
 
 
+
+
+
 module.exports = defineConfig({
   viewportHeight: 720,
   viewportWidth: 1600,
   e2e: {},
+  projectId: "2scvuw",
   video: true,
 })
